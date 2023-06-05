2731a90a1a479a441039d04339c865fb6cc29914
ashwabu <abdulazizredi90@gmail.com>
2023-06-05 10:55:25 +0300

    asking for close matches

--- dictionary.py
+++ dictionary.py
@@ -6,9 +6,13 @@ data = json.load(open('data.json'))
 def dictionary(word):
     if word in data.keys():
         return data[word]
-    elif len(word,get_close_matches(data.keys())) > 0:
-        
-        return get_close_matches(data.keys()[0])
+    elif len(get_close_matches(word,data.keys())) > 0:
+        conferm = input(f'Do you mean {get_close_matches(word,data.keys())[0]}?\ny for yes or n for no: ')
+        conferm = conferm.lower()
+        if conferm == 'y':
+            return data[get_close_matches(word,data.keys())[0]]
+        else:
+            return 'pleade check your spealing and try again'
     else:
         return 'Please cheak your spealing'
 
