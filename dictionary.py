19c725cfe063c75e5db086ebac318700cbbdd80f
ashwabu <abdulazizredi90@gmail.com>
2023-06-05 10:44:12 +0300

    the output is better

--- dictionary.py
+++ dictionary.py
@@ -6,10 +6,17 @@ data = json.load(open('data.json'))
 def dictionary(word):
     if word in data.keys():
         return data[word]
+    elif len(word,get_close_matches(data.keys())) > 0:
+        
+        return get_close_matches(data.keys()[0])
     else:
         return 'Please cheak your spealing'
 
 
 word = input('Enter a word: ')
-
-print(dictionary(word))
+output = dictionary(word)
+if type(output) == list:
+    for op in output:
+        print(op)
+else:
+    print(output)
